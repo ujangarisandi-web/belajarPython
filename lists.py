2bc54c632380805d3afd9dce6024b3b7fa44ab36
ujangaja <ujangaja@gmail.com>
2019-10-04 20:01:12 +0700

    update latihan

---
+++ lists.py
@@ -0,0 +1,6 @@
+data = [1,4,9,16, 25, 36, 49, 64]
+
+subdata1 = data[3]
+subdata2 = data[-3]
+subdata3 = data[2:4]
+print(subdata3)
---
+++ string1.py
@@ -0,0 +1,22 @@
+text1 = 'jalan- jalan di hari minggu'
+text2 = 'jalan- jalan di hari jum\'at'
+text3 = "jalan- jalan di hari jum'at"
+text4 = 'mahmuy berkata, "kemarin kemana bro?"'
+text5 = "hoblah berkata,\' jalan-jalan bro\' "
+text6 = 'mahmuy : "kemarin kemana bro?" \n hoblah: "jalan-jalan bro"'
+
+text7 = """"
+mahmuy : "kemarin kemana bro?"
+hoblah : "jalan-jalan bro"
+mahmuy : "jalan- jalan kemana bro?"
+hoblah : "ke mang Uing bro!!!"
+"""
+
+text8 = r'C:\nyoto'#raw string untuk huruf r
+print(text8)
+
+#ada yag seru
+print(50*'wk')
+
+print('kue' 'pukis')
+print(text1+ text2)
