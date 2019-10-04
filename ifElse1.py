1bf16f7829a8236995d5f93a4712398815d7940d
ujangaja <ujangaja@gmail.com>
2019-10-04 21:25:36 +0700

    ifelse pada python

---
+++ ifElse1.py
@@ -0,0 +1,21 @@
+nilai = 50
+
+if nilai == 75:  #equal eksplisit
+    print('nilai anda', nilai)
+
+if nilai is 60:  #equal
+    print('nilai anda', nilai)
+if nilai is not 60:  #not equal
+    print('nilai anda bukan : 60')
+print(50 * '=')
+nilai = 60
+if 80 <= nilai <= 100:
+    print('nilai anda adalah A')
+elif 70 <= nilai < 80:
+    print('nilai anda adalah B')
+elif 60 <= nilai < 70:
+    print('nilai anda adalah C')
+elif 50 <= nilai < 60:
+    print('nilai anda adalah T, lakukan pebaikan')
+else:
+    print('maaf anda tidak lulus matakuliah ini')
--- numbers.py
+++ numbers.py
@@ -1,15 +1,15 @@
 a = 5
 b = 3
-c =4.0
+c = 4.0
 
 #penjumlahan
-print('penjumlahan a+ b =',a+b)
+print('penjumlahan a+ b =', a + b)
 
 #penguranga
-print('penguranga a - b =',a-b)
+print('penguranga a - b =', a - b)
 
 #perkalian
-print('perkalian a* b =',a*b)
+print('perkalian a* b =', a * b)
 
 #penjumlahan
-print('pembagian a /b =',a/b)
+print('pembagian a /b =', a / b)
