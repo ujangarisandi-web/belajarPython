5c7072d67f60f8e803c5fd4f2c96b2b642e75813
ujangaja <ujangaja@gmail.com>
2019-10-07 20:16:08 +0700

    if else2

--- ifElse1.py
+++ ifElse1.py
@@ -19,3 +19,26 @@ elif 50 <= nilai < 60:
     print('nilai anda adalah T, lakukan pebaikan')
 else:
     print('maaf anda tidak lulus matakuliah ini')
+
+print(50 * '=')
+print('operator logika untuk list dan string')
+print(' ')
+
+gorengan = [
+    'bakwan', 'cireng', 'bala-bala', 'gehu', 'combro', 'pisang goreng',
+    'pukis', 'risoles'
+]
+beli = 'bandros'
+
+if beli in gorengan:
+    print('Mamang bilang, "ya, saya jual', beli, '"')
+if not beli in gorengan:
+    print('"saya gak jual', beli, '"')
+
+print(50 * '=')
+
+karakter = 'z'
+if karakter in beli:
+    print('ada', karakter, 'di', beli)
+else:
+    print('tidak ada', karakter, 'di', beli)
